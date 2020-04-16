9fda17e0c37ccf07ffa4eb7b10600876de787e1e
Freitas <RODRIGO.FREITAS@sicpa.com>
2020-04-16 16:07:31 -0300

    Requisições
    - Criada uma requisição para pegar informações de um json
    - Protótipo para utilizar no meu site
    - Colocado no meu servidor por problemas de CORS

--- pokedex/js/services/pokeApiFactory.js
+++ pokedex/js/services/pokeApiFactory.js
@@ -1,14 +1,33 @@
 angular.module('pokedexApp')
 .factory('PokeApiFactory', PokeApiFactory);
 
-function PokeApiFactory(){
+PokeApiFactory.$inject = ['$http'];
+
+function PokeApiFactory($http){
     return {
-        pkmList : [
-            {name : 'Bulbasaur', number  : 1},
-            {name : 'Pikachu', number  : 25},
-            {name : 'Mew', number  : 151},
-            {name : 'Chikorita', number  : 152},
-            {name : 'Celebi', number  : 251},
-        ]
+        get url(){
+            return '//dev.treinaweb.com.br/pokeapi/'
+        },
+        pkmList : [],
+        listAll : function(){
+            return $http.get(`${this.url}pokedex/1`)
+            .then(response => response.data.pokemon)
+            .then(pkmList => {
+                return this.pkmList.map(pokemon => {
+                    pokemon.number = this.getNumberFromURL(pokemon.resource_uri);
+                    return pokemon;
+                })
+                .filter(pokemon => pokemon.number < 1000)
+                .sort((a, b) => (a.number < b.number ? 1 : -1))
+            })
+            .then(pkmList => {
+                this.pkmList = pkmList;
+                return pkmList;
+            })
+             
+        },
+        getNumberFromURL : function(url){
+            return parseInt(url.replace(/.*\/(\d+)\/$/, '$1'));
+        }
     }
 }
